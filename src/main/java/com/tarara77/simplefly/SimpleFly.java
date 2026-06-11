a5babe1b3538a266a5ce4ecd7fda81865603e0d5
Sabbir <93143691+s8rr@users.noreply.github.com>
2026-06-11 22:28:29 +0600

    Update SimpleFly.java

--- src/main/java/com/tarara77/simplefly/SimpleFly.java
+++ src/main/java/com/tarara77/simplefly/SimpleFly.java
@@ -1,1 +1,18 @@
+package com.tarara77.simplefly;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SimpleFly extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        if (this.getCommand("fly") != null) {
+            this.getCommand("fly").setExecutor(new FlyCommand());
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
